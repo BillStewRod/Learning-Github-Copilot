acc6630d83bfda190e516bdc42b0c8bad8a12c38
Billiam Stewart Rodriguez <billstewrod@icloud.com>
2024-04-06 16:53:49 -0400

    convert.py

---
+++ convert.py
@@ -0,0 +1,12 @@
+import os
+
+# Read mp3 files from the audio directory and list them
+def read_mp3_files():
+    audio_files = []
+    for file in os.listdir('audio'):
+        if file.endswith('.mp3'):
+            audio_files.append(file)
+    return audio_files
+
+print(read_mp3_files())
+
